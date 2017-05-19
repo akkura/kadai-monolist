d2606b1900458fe2ca860462501e42740a8bad81
ao <ao72aku@gmail.com>
2017-05-19 06:08:38 +0000

    have,wantranking

---
+++ app/Http/Controllers/RankingController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Item;
+
+class RankingController extends Controller
+{
+     public function want()
+    {
+        $items = [];
+        if (Item::exists()) {
+            $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as count'))->where('type', 'want')->groupBy('items.id')->orderBy('count', 'DESC')->take(10)->get();
+        }
+
+        return view('ranking.want', [
+            'items' => $items,
+        ]);
+    }
+   
+    public function index()
+    {
+        //
+    }
+
+   
+    public function create()
+    {
+        //
+    }
+
+   
+    public function store(Request $request)
+    {
+        //
+    }
+
+  
+    public function show($id)
+    {
+        //
+    }
+
+  
+    public function edit($id)
+    {
+        //
+    }
+
+  
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+   
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -35,9 +35,10 @@ class UsersController extends Controller
    
     public function show($id)
     {
-        if(Auth::user()->id == $id){
+      
          $user = User::find($id);
         $count_want = $user->want_items()->count();
+        $count_have = $user->have_items()->count();
         $items = [];
         if (Item::exists()) {
             $items = \DB::table('items')->join('item_user', 'items.id', '=', 'item_user.item_id')->select('items.*')->where('item_user.user_id', $user->id)->distinct()->groupBy('items.id')->paginate(20);
@@ -47,11 +48,9 @@ class UsersController extends Controller
             'user' => $user,
             'items' => $items,
             'count_want' => $count_want,
-            /**'count_have' => $count_have,**/
+            'count_have' => $count_have,
         ]);
-        }
-        
-        return redirect()->back();
+     
     }
 
   
